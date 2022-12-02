8dba741fc0096e254381349ea733ee5d03dd194c
Brandon <bbeam1@gmail.com>
2022-12-01 21:28:36 -0700

    finished login checker

--- express_server.js
+++ express_server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser')
-const users = {}
+const users = { fy7dh7: { id: 'fy7dh7', email: 'bbeam1@gmail.com', password: 's' } }
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca", userId: 'fy7dh7'
+  },
+  "9sm5xK": { longURL: "http://www.google.com", userId: 'fdsgh7' }
 };
 
 const emailFinder = (email) => {
@@ -28,72 +30,125 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/urls", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const username = users[user_id]
-  const templateVars = { username, urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  if (req.cookies.user_id) {
+    const user_id = req.cookies.user_id;
+    const username = users[user_id]
+    const yourUrls = () => {
+      let personalURLS = {}
+      for (const item in urlDatabase) {
+        if (urlDatabase[item].userId === user_id)
+          personalURLS[item] = urlDatabase[item]
+        console.log(personalURLS)
+      } return personalURLS
+    }
+    let urls = yourUrls(user_id)
+    console.log(urls)
+    const templateVars = { username, urls };
+    res.render("urls_index", templateVars);
+  } else {
+    res.status(404).send('not logged in')
+  }
 });
 
 app.get("/urls/new", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const username = users[user_id]
-  const templateVars = {
-    username,
-  };
-  res.render("urls_new", templateVars);
+  if (req.cookies.user_id) {
+    const user_id = req.cookies.user_id;
+    const username = users[user_id]
+    const templateVars = {
+      username,
+    };
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login")
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const username = users[user_id]
-  const templateVars = { username, id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
+  const longURL = urlDatabase[req.params.id].longURL
+  if (longURL && req.cookies.user_id === urlDatabase[req.params.id].userId) {
+    const user_id = req.cookies.user_id;
+    const username = users[user_id]
+    const templateVars = { username, id: req.params.id, longURL };
+    res.render("urls_show", templateVars);
+  } if (req.cookies.user_id !== urlDatabase[req.params.id].userId) { //wanted a different error message for this. technically couldve gone to else
+    res.status(404).send('you shouldnt be here')
+  }
+  else {
+    res.sendStatus(404)
+  }
 });
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.id].longURL
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.sendStatus(404)
+  }
 });
 
 app.get("/registration", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const username = users[user_id]
-  const templateVars = { username }
-  res.render("registration", templateVars);
+  if (!req.cookies.user_id) {
+    const user_id = req.cookies.user_id;
+    const username = users[user_id]
+    const templateVars = { username }
+    res.render("registration", templateVars);
+  } else {
+    res.redirect("/urls")
+  }
 });
 
 app.get("/login", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const username = users[user_id]
-  const templateVars = { username }
-  res.render("login", templateVars);
+  if (!req.cookies.user_id) {
+    const user_id = req.cookies.user_id;
+    const username = users[user_id]
+    const templateVars = { username }
+    res.render("login", templateVars);
+  } else {
+    res.redirect("/urls")
+  }
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  const newstring = generateRandomString()
-  urlDatabase[newstring] = req.body.longURL
-  console.log(urlDatabase)
-  res.redirect(302, `/urls/${newstring}`)
+  if (req.cookies.user_id) {
+    const newstring = generateRandomString()
+    const longURL = req.body.longURL
+    userId = req.cookies.user_id
+    urlDatabase[newstring] = { longURL, userId }
+    res.redirect(302, `/urls/${newstring}`)
+  } else {
+    res.status(403).send('nice try')
+  }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect("/urls")
+  const user = req.cookies.user_id
+  const id = req.params.id
+  if (!user) {
+    res.status(404).send('account does not exist or is not logged in')
+  } else if (!urlDatabase[id]) {
+    res.status(404).send('link not found')
+  } else if (user !== urlDatabase[id].userId) {
+    res.status(404).send('no access allowed')
+  } else {
+    delete urlDatabase[req.params.id]
+    res.redirect("/urls")
+  }
 });
 
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL
-  res.redirect("/urls")
+  const user = req.cookies.user_id
+  const id = req.params.id
+  if (!user) {
+    res.status(404).send('account does not exist or is not logged in')
+  } else if (!urlDatabase[id]) {
+    res.status(404).send('link not found')
+  } else if (user !== urlDatabase[id].userId) {
+    res.status(404).send('no access allowed')
+  } else {
+    urlDatabase[req.params.id].longURL = req.body.longURL
+    res.redirect("/urls")
+  }
 });
 
 app.post("/login", (req, res) => {
